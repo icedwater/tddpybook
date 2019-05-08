d1eb941c9a67d428a4b4c22eba3b3572f9083125
icedwater <icedwater@gmail.com>
2019-05-08 14:32:43 +0800

    Updated tests to check for To-Do.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,27 @@
 from selenium import webdriver
 
+# XY sets up the browser and loads the to-do list app
 browser = webdriver.Firefox()
 browser.get("http://localhost:8000")
+
+# He checks that the page title is correct
+assert "To-Do" in browser.title
+
+# He adds a to-do list item: "book karaoke room"
+
+# XY notes that pressing enter updates the page with the item:
+# "1. book karaoke room"
+
+# The text box below allows another item to be added
+# so XY adds another: "invite friends to karaoke"
+
+# The page is updated with both items.
+
+# XY checks that the site has a custom URL for the list.
+
+# XY checks that the custom URL holds the correct list.
+
+# OK, time to do other stuff.
+browser.quit()
+
+
