663b97b0e42916bf77bf94f5ec0aef270a0f6228
icedwater <icedwater@gmail.com>
2019-05-09 17:32:18 +0800

    Refactored home page view to use a template
    - used HTML5 version of template
    - tested for render output rather than constants
    - installed "lists" app

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    htmlpage = """<html>
-    <head>
-    <title>To-Do Lists</title>
-    </head>
-    </html>"""
-    return HttpResponse(htmlpage)
+    return render(request, "home.html")
