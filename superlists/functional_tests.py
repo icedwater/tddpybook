f0db7df9b8caa30c3401a86abcabc6a3614f176c
icedwater <icedwater@gmail.com>
2019-05-09 17:08:37 +0800

    Updated functional test with new items
    - can check for "To-Do" in h1 element as well as title
    - adds a first item and checks that it is there
    - adds a second item and checks that second row is there
    - self.fail bookmark added

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -24,19 +24,41 @@ class NewVisitorTest(unittest.TestCase):
 
         # He checks that the page title is correct
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Finish the test.")
+
+        # He notices that the word "To-Do" occurs in the first page header
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("To-Do", header_text)
+
+        # XY sees that there is a text box to enter a to-do item
+        inputbox = self.browser.find_element_by_id("new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "What next?")
 
         # He adds a to-do list item: "book karaoke room"
+        inputbox.send_keys("book karaoke room")
+        inputbox.send_keys(Keys.ENTER)
 
         # XY notes that pressing enter updates the page with the item:
         # "1. book karaoke room"
+        table = self.browser.find_element_by_id("to_do_list")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(
+                any(row.text == "1: book karaoke room" for row in rows)
+        )
 
         # The text box below allows another item to be added
         # so XY adds another: "invite friends to karaoke"
+        inputbox.send_keys("invite friends to karaoke")
+        inputbox.send_keys(keys.ENTER)
 
         # The page is updated with both items.
+        table = self.browser.find_element_by_id("to_do_list")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(
+                any(row.text == "2: invite friends to karaoke" for row in rows)
+        )
 
         # XY checks that the site has a custom URL for the list.
+        self.fail("Test will fail here!")
 
         # XY checks that the custom URL holds the correct list.
 
