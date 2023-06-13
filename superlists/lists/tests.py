50932818019eca9231ea25dea1182cf0d96fdead
icedwater <icedwater@gmail.com>
2023-06-14 02:10:40 +0800

    Added meatier HTML homepage and updated test.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 # Create your tests here.
@@ -9,3 +11,15 @@ class HomePageTest(TestCase):
     def test_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode("utf8")
+        self.assertTrue(html.startswith("<html>"))
+        self.assertIn("<head>", html)
+        self.assertIn("</head>", html)
+        self.assertIn("<body>", html)
+        self.assertIn("<title>To-Do lists</title>", html)
+        self.assertIn("</body>", html)
+        self.assertTrue(html.endswith("</html>"))
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse("""<html>
+        <head>
+            <title>To-Do lists</title>
+        </head>
+        <body>
+            <h1>To-Do</h1>
+            <input id = "new_item" placeholder = "What next?"></input>
+        </body>
+    </html>""")
